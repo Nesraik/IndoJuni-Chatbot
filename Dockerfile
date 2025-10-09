d3c275d39ec8129d7e9304f4e08f186a6eae0ad3
Calvin Farrellino Kurniawan <calvinfarrellinok@gmail.com>
2025-10-09 11:14:17 +0700

    get access key

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ FROM indojuni-chatbot:base
 COPY . .
 
 EXPOSE 8114
-CMD ["python", "app.py"]
+CMD ["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "8114"]
--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI, Request
-
+# uvicorn app:app --host 0.0.0.0 --port 8114 --reload
 app = FastAPI()
 
 
 # Get user prompt
 @app.post("/api/v1/chat")
 async def chat(request: Request):
+    access_key_bearer: str = request.headers.get("Authorization", "")
+    if access_key_bearer == "":
+        return {"error": "Unauthorized"}, 401
+    access_key = access_key_bearer.split(" ")[1]
+    print(f"Access Key: {access_key}")
     return [
         {
             "role": "agent",
@@ -15,9 +20,15 @@ async def chat(request: Request):
 
 
 @app.get("/api/v1/init")
-async def init():
+async def init(request: Request):
+    access_key_bearer: str = request.headers.get("Authorization", "")
+    if access_key_bearer == "":
+        return {"error": "Unauthorized"}, 401
+    access_key = access_key_bearer.split(" ")[1]
+    print(f"Access Key: {access_key}")
     return [
         {
-            "status": "initialized"
+            "role": "agent",
+            "content": "Hello! How can I assist you today?",
         }
     ]
